e82d6398cf11d6a9761bcb3622269f38ad54b2e2
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-02-11 16:24:10 -0800

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -1,1 +1,11 @@
---
+blacklisted = {}
+
+if table.find(blacklisted, game:GetService("Players").LocalPlayer.UserId) then
+      game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Blacklisted from oblivion",
+        Text = "FUCK OFF",
+        Duration = 999
+    })
+  wait(0.5)
+    while true do end
+end
