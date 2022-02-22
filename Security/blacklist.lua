d981e3041380a0aabea6feebf37685267028d8ff
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-02-21 21:09:36 -0800

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -5,7 +5,7 @@ Blacklisted users
 ]]
 blacklisted = {
 1516191706, --Incopetent kid 
-
+1855278397, --Retarded softvortex user who claims that its the best script
 }
 
 if table.find(blacklisted, game:GetService("Players").LocalPlayer.UserId) then
