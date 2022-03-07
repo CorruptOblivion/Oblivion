76ea9ffc1988ece04bd643e68c86d4a84e11dad4
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-03-06 18:24:51 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,5 +5,12 @@ if game.PlaceId == 5602055394 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptOblivion/Oblivion/main/Games/DaHoodModded.lua"))()
 end
 if game.PlaceId == 8114670837 then
+    --[[
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptOblivion/Oblivion/main/Games/DeHood.lua"))()
+    ]]
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "uh oh",
+        Text = "Oblivion no longer works with de hood, may be fixed in the future.",
+        Duration = 999
+    })
 end
