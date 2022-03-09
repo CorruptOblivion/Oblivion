7a4826dfe3e29c1a398cd7c3f3d75b8055cb0d77
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-03-08 23:35:49 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,11 @@
+if game:GetService("CoreGui"):FindFirstChild("Oblivion") then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Oblivion",
+        Text = "You only need to execute once. This will lag your game.",
+        Duration = 25
+    })    
+end
+
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptOblivion/Oblivion/main/Games/DaHood.lua"))()
 end
