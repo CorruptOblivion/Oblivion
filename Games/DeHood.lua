0f9c184e009f6dc1372c6047b6ba09c5c102223b
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-05-30 10:25:56 -0700

    Update DeHood.lua

--- Games/DeHood.lua
+++ Games/DeHood.lua
@@ -1,1 +1,5 @@
-
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "Oblivion",
+  Text = "This shit game died so long ago I dont get why i made this lmao",
+  Duration = 25
+})    
