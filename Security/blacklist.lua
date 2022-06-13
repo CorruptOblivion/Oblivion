0270989d72fe02707d89593842f5eaf2d1bfe290
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-06-13 14:59:14 -0700

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -3,17 +3,16 @@
 Blacklisted users
 
 ]]
-blacklisted = {
-1516191706, --Incopetent kid 
-1855278397,
-}
 
-if table.find(blacklisted, game:GetService("Players").LocalPlayer.UserId) then
-      game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Blacklisted from Oblivion",
-        Text = "FUCK OFF",
-        Duration = 999
-    })
-  wait(0.5)
-    while true do end
+local player = game:GetService('Players').LocalPlayer
+
+local die = function(args)
+ player:Kick('\nYour account has been blacklisted from using Oblivion for the following reason: ' .. args)
+ while true do end
+end
+
+if player.UserId == 1576756053 then
+     die('Chariotsware meat rider, problematic')
+elseif player.UserId == 1894690748 then -- test
+      die('Test')
 end
