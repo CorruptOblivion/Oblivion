ddd269601ef884ae6157bdd23349fd475852c1d5
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-06-13 15:04:38 -0700

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -7,7 +7,7 @@ Blacklisted users
 local player = game:GetService('Players').LocalPlayer
 
 local die = function(args)
- player:Kick('\nYour account has been blacklisted from using Oblivion for the following reason: ' .. args)
+ player:Kick('\nYour account has been blacklisted from using Oblivion for the following reason: ' .. args .. '\n If you think this is a mistake, send this to CorruptOblivion#7914')
  wait(0.5)
  while true do end
 end
