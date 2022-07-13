fcb719232d41433ab1233988007ec45228ccc2c2
VERT <96640962+VERTVERTVERTVERTVERTVERTVERTVERTVERT@users.noreply.github.com>
2022-07-13 13:12:51 -0700

    Update DaHood.lua

--- Games/DaHood.lua
+++ Games/DaHood.lua
@@ -2,4 +2,5 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "Oblivion",
   Text = "Not avalible | This script is for hood modded only at the moment",
   Duration = 25
+  Icon = 'rbxassetid://9423555621'
 })    
