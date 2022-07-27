54208ca9d78828ed3d2f1551b92bff095f176820
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-07-27 14:56:53 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,7 +11,7 @@ end
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "Oblivion",
-  Text = "Loading Oblivion [Semi Broken]",
+  Text = "Loading Oblivion",
   Duration = 5,
   Icon = 'rbxassetid://9423555621'
 }) 
