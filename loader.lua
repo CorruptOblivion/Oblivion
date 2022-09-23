b6a1ae15c48b819fcef67ea9db8f7abc9abef2f6
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-09-22 22:43:32 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,7 +12,7 @@ end
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "Oblivion",
-  Text = "[NOTICE] Please DM CorruptOblivion#7914 if you are having issues with using Fluxus or KRNL.",
+  Text = "[NOTICE] Please DM CorruptOblivion#7914 if you are having issues with using Fluxus, KRNL or any other free executor",
   Duration = 5,
   Icon = 'rbxassetid://9423555621'
 }) 
