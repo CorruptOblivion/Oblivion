6c185eaabe827bef23fc0d0c02760d67bdcbe9f9
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-10-13 17:19:51 -0700

    Update DaHood.lua

--- Games/DaHood.lua
+++ Games/DaHood.lua
@@ -1,6 +1,6 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {
-  Title = "Oblivion",
-  Text = "Not avalible | This script is for hood modded only at the moment",
+game:GetService('StarterGui'):SetCore('SendNotification', {
+  Title = 'Oblivion',
+  Text = 'Not avalible | This script is for hood modded only at the moment',
+  Icon = 'rbxassetid://9423555621',
   Duration = 25,
-  Icon = 'rbxassetid://9423555621'
 })    
