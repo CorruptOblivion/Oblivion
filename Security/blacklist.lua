c973edaa19915773924f2062ed2337f5428f0298
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-11-30 15:26:00 -0800

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -15,3 +15,7 @@ end
 if player.UserId == 1576756053 then
      die('Chariotsware meat rider, problematic')
 end
+
+if player.UserId == 2205701910 then
+  die('KILL YOURSELF AND STOP RIPPING SHIT FROM MY REPOSITORY')
+end 
