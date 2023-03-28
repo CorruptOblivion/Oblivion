a7ff63e2a39b40bed1b7d3c165078e5ea19022da
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2023-03-27 21:26:30 -0500

    Update blacklist.lua

--- Security/blacklist.lua
+++ Security/blacklist.lua
@@ -11,6 +11,7 @@ local BL_TBL = {
  [2205701910] = 'KILL YOURSELF AND STOP RIPPING SHIT FROM MY REPOSITORY',
  [3731002752] = 'Sending CP in my server with an alt. PS.. You did a terrible job at covering your tracks',
  [2704543218] = 'Crack attempt (x13)',
+ [2671971680] = 'Retarded',
 }
 
 return BL_TBL
